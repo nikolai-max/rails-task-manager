ea4649950c7d67ce449ab86518853d4a2ffbc79c
Nikolai Toverud <nikolai.toverud@gmail.com>
2020-02-11 16:09:58 +0100

    Add routes, controller methods and views for CRUD. Still some refactoring to be done, and styling is missing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "tasks" to: "tasks#index" as: "tasks"
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/new', to: 'tasks#new', as: :new
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
+  # resources :restaurants
 end
-
-
-# First, add a new route to list the tasks, following the convention from the lecture. Then, add a controller action and its view. This action should fetch all tasks, and a view should loop over these to display them, like in the screenshot below.
-
-# To test your view, you need some tasks in the database! To create some, run a rails console in another terminal tab and then run:
